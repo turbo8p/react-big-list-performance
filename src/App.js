c4a04e3f0ec2b41e327d96bcbbb4fccea640a6eb
Turbo <p.petpadriew@gmail.com>
2021-11-11 10:52:26 +0700

    feat: use reducer use case

--- src/App.js
+++ src/App.js
@@ -1,157 +1,87 @@
-import './App.css';
-import {useState, useEffect} from "react"
-import { Profiler } from 'react';
+import "./App.css";
+import { useCallback,useEffect, useReducer } from "react";
 
+const undoReducer = (state, action) => {
+  switch(action.type) {
+    case 'UNDO':
+      if (state.past.length <= 0) {
+        return state;
+      }
+      return {
+        ...state,
+        past: state.past.slice(0, state.past.length - 1),
+        present: state.past[state.past.length - 1],
+        future: [state.present, ...state.future]
+      };
+    case 'REDO':
+      if(state.future.length <= 0) {
+        return state;
+      }
+      return {
+        ...state,
+        past: [...state.past, state.present],
+        present: state.future[0],
+        future: state.future.slice(1)
+      };
+    case 'SET':
+      return {
+        past: [...state.past, state.present],
+        present: action.payload.newPresent,
+        future: [],
+      };
+    case 'RESET':
+      return {
+        past: [],
+        present: action.payload.initialPresent,
+        future: []
+      };
+    default:
+      throw new Error('Unhandled action type: ' + action.type);
+  }
+};
 
-// function TodoItem (props) {
-//   const {id, done, text, todo, onTodoUpdate, dummyArray} = props;
-
-
-//   // const todo = useSelector((state) => {
-//   //     return state.shippingCenterPage.todos.find(todo => todo.id === props.id)
-//   // }, shallowEqual);
-//   const handleCheckboxChange = useCallback((e) => {
-//       // dispatch({type: 'UPDATE_TODO', todo: {id, text, done: e.target.value}});
-//       onTodoUpdate({dummyArray, id, text, done: e.target.value});
-//   });
-//   const handleTextChange = useCallback((e) => {
-//       // dispatch({type: 'UPDATE_TODO', todo: {id, text: e.target.value, done}});
-//       onTodoUpdate({dummyArray, id, done, text: e.target.value});
-//   });
-//   const handleBtnClick = useCallback(() => {
-//       onTodoUpdate({id, done, text, dummyArray: [{name: "cat"}]});
-//   });
-
-//   console.log(`Todo item:${text}`);
-//   return (
-//       <li>
-//           <input type="checkbox" checked={done} onChange={handleCheckboxChange}/>
-//           <input type="text" value={text} onChange={handleTextChange}/>
-//           <button onClick={handleBtnClick}>{todo.dummyArray[0].name}</button>
-//       </li>
-//   );
-// }
-
-const dummyTodos = [];
-const nestedItems = [];
-for(let i = 0; i<=20; i+=1) {
-  nestedItems.push({
-    id: Math.random().toString(16).slice(2),
-    completed: false, 
-    text: i,
-    items: []
+function useUndo(initialPresent) {
+  const [state, dispatch] = useReducer(undoReducer, {
+    past: [],
+    present: initialPresent,
+    future: [],
   });
-}
 
-for(let i = 0; i<=10; i+=1) {
-  dummyTodos.push({
-    id: Math.random().toString(16).slice(2),
-    completed: false, 
-    text: i,
-    items: nestedItems
-  });
-}
+  const undo = useCallback(() => {
+    dispatch({type: 'UNDO'});
 
+  }, []);
 
-function TodoItem(props) {
-  const {id, completed, text, onCompletedChange, onTextChange} = props;
-  return (
-    <>
-      <input type="checkbox" checked={completed} onChange={(e) => onCompletedChange(id, e.target.checked)}/>
-      <input type="text" value={text} onChange={(e) => onTextChange(id, e.target.value)} />
-    </>
-  );
+  const redo = useCallback(() => {
+    dispatch({type: 'REDO'});
+  }, []);
+
+  const set = useCallback((newPresent) => {
+    dispatch({type: 'SET', payload: {newPresent}});
+  }, []);
+
+  const reset = useCallback(() => {
+    dispatch({type: 'RESET', payload: {initialPresent}});
+  }, []);
+
+  return [
+    state,
+    {set, reset, undo, redo}
+  ];
 }
 
 function App() {
-  const [todos, setTodos] = useState(dummyTodos);
-  const [todoInput, setTodoInput] = useState('');
+  const [state, {set, undo, redo}] = useUndo('first');
+  
+  useEffect(() => {
+    set('second');
+    set('third');
+    undo();
+    redo();
+  }, [set, undo, redo]);
 
-  const handleInputKeyPress = (e) => {
-      if(e.key === 'Enter') {
-        setTodos([
-          ...todos, 
-          {id: Math.random().toString(16).slice(2),completed: false, text: todoInput}
-        ]);
-        setTodoInput('');
-      }
-  };
 
-  const handleInputChange = (e) => {
-    setTodoInput(e.target.value);
-  };
-
-  const handleTodoTextChange = (id, text, childId) => {
-
-      setTodos(todos.map((todo) => {
-        if(todo.id === id) {
-          let items = todo.items;
-          if(childId) {
-            items = todo.items.map((item) => {
-              if(item.id === childId) {
-                return {...item, text: text};
-              }
-              return item;
-            });
-
-            return {...todo, items};
-          }
-
-          return  {...todo, text};
-        }
-        return todo;
-      }));
-  }
-
-  const handleTodoCompletedChange = (id, completed, childId) => {
-    setTodos(todos.map((todo) => {
-      if(todo.id === id) {
-        return {...todo, completed};
-      }
-      return todo;
-    }));
-  };
-
-  return (
-    <div className="App">
-        <input type="text" 
-          placeholder="To do" 
-          onKeyPress={handleInputKeyPress} 
-          onChange={handleInputChange}
-          value={todoInput}
-        />
-        {/* <Profiler id="todo-profiler" onRender={(id, phase, actualDuration, baseDuration, startTime, commitTime, interactions) => {
-          console.log(phase);
-          console.log(actualDuration);
-
-        }} > */}
-{todos.map((todo, index) => {
-              return (
-                <div key={index}>
-                  <TodoItem 
-                    id={todo.id}
-                    completed={todo.completed}
-                    text={todo.text}
-                    onCompletedChange={handleTodoCompletedChange}
-                    onTextChange={handleTodoTextChange}
-                  />
-                  <ul>
-                    {todo.items.map((item) => {
-                      return (
-                        <li style={{'backgroundColor': 'red'}}>
-                          <input type="checkbox" checked={item.completed} onChange={(e) => handleTodoCompletedChange(todo.id, e.target.checked, item.id)}/>
-                          <input type="text" value={item.text} onChange={(e) => handleTodoTextChange(todo.id, e.target.value, item.id)} />
-                        </li>
-                        );
-                    })}
-                  </ul>
-                </div>
-              );
-          })}
-        {/* </Profiler> */}
-        
-    </div>
-  );
+  return <pre>{JSON.stringify(state, null, 2)}</pre>;
 }
 
 export default App;
